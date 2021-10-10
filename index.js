ba4b8e267392ce97a4c08d5fc951e33cff2c25fe
Taras Boikov <tarasgowest@gmail.com>
2021-10-10 22:22:13 +0300

    добавляет таймер

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+const refs = {
+    daysArea: document.querySelector('[data-value="days"]'),
+    hoursArea: document.querySelector('[data-value="hours"]'),
+    minArea: document.querySelector('[data-value="mins"]'),
+    secArea: document.querySelector('[data-value="secs"]'),
+}
+
+
 const timer = {
     start() {
-        const startTime = Date.now();
+        const targetDate = new Date('Jan 01, 2022');
         setInterval(() => {
-            const currentTime = Date.now();
-            console.log('start -> currentTime', currentTime)
+            const currentDate = Date.now();
+            const timerSet = targetDate - currentDate;
+            const { days, hours, mins, secs } = newYearTimer(timerSet);
+            refs.daysArea.textContent = `${days}`;
+            refs.hoursArea.textContent = `${hours}`;
+            refs.minArea.textContent = `${mins}`;
+            refs.secArea.textContent = `${ secs }`;
         }, 1000);
     }
 };
 
 timer.start();
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+
+function newYearTimer(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+};
